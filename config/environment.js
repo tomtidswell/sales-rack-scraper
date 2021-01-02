dc303e982c0c73b5507bbdd781cbf7cdca001e6f
tomtidswell <tomtidswell@gmail.com>
2021-01-02 16:48:21 +0000

    cleaning up repo

--- config/environment.js
+++
@@ -1,7 +0,0 @@
-// the or statements here allow a default if they havent been set with a .env file (ie in a production env)
-
-const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/sales'
-const secret = process.env.SECRET || 'tommykins rules ok'
-
-module.exports = { port, dbURI, secret }
